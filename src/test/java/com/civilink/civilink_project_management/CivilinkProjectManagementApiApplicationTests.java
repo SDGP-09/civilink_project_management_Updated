41cffdc3523d466055a15891e98e08112fc91f08
AyendriJayasekara <jayasekaraayendri@gmail.com>
2025-01-29 22:18:25 +0530

    Initial commit

---
+++ src/test/java/com/civilink/civilink_project_management/CivilinkProjectManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civilink.civilink_project_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivilinkProjectManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
