426eb43c71a1feedccaff35408221674dbb75b0d
Lasith Hettiarachchi <lasith.hettiarachchi.dev@gmail.com>
2025-02-09 19:32:10 +0530

    - DeleteAllbyMainTaskId method code changed - Mysql password changed to docker mysql root password - Discovery client enabled.

--- src/main/java/com/civilink/civilink_project_management/repositories/SubTaskRepository.java
+++ src/main/java/com/civilink/civilink_project_management/repositories/SubTaskRepository.java
@@ -2,12 +2,27 @@ package com.civilink.civilink_project_management.repositories;
 
 import com.civilink.civilink_project_management.entities.MainTask;
 import com.civilink.civilink_project_management.entities.SubTask;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface SubTaskRepository extends JpaRepository<SubTask, Long> {
     void deleteAllByMainTask(MainTask mainTask);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM sub_task WHERE main_task_id = ?1", nativeQuery = true)
+    void deleteSubTaskByMainTaskId(Long Id);
+
+    List<SubTask> findAllByMainTaskId(Long id);
+
+
 }
--- src/main/java/com/civilink/civilink_project_management/services/Impl/DeleteSubTaskImpl.java
+++ src/main/java/com/civilink/civilink_project_management/services/Impl/DeleteSubTaskImpl.java
@@ -5,10 +5,14 @@ import com.civilink.civilink_project_management.entities.SubTask;
 import com.civilink.civilink_project_management.repositories.MainTaskRepository;
 import com.civilink.civilink_project_management.repositories.SubTaskRepository;
 import com.civilink.civilink_project_management.services.DeleteSubTaskService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +21,11 @@ public class DeleteSubTaskImpl implements DeleteSubTaskService {
 
     private final SubTaskRepository subTaskRepository;
     private final MainTaskRepository mainTaskRepository;
+
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
 
     @Autowired
     public DeleteSubTaskImpl(SubTaskRepository subTaskRepository, MainTaskRepository mainTaskRepository) {
@@ -41,11 +50,10 @@ public class DeleteSubTaskImpl implements DeleteSubTaskService {
     @Override
     @Transactional
     public void deleteAllSubTasks(Long mainTaskId) {
-        Optional<MainTask> mainTask = mainTaskRepository.findById(mainTaskId);
 
-        if (mainTask.isPresent()) {
-            System.out.println("Deleting SubTask: " );
-            subTaskRepository.deleteAllByMainTask(mainTask.get());
+
+       if(mainTaskId != null){
+            subTaskRepository.deleteSubTaskByMainTaskId(mainTaskId);
         } else {
             throw new RuntimeException("MainTask with ID " + mainTaskId + " not found.");
         }
